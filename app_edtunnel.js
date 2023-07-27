da87142d556b436161aedfff1c4573226ed4a1ab
jiada66 <43734570+jiada66@users.noreply.github.com>
2023-07-28 07:21:05 +0800

    Update app_edtunnel.js

--- app_edtunnel.js
+++ app_edtunnel.js
@@ -7,7 +7,7 @@ const wss = new WebSocket.Server({ port: 443 });
 let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
 let proxyIP = "64.68.192." + Math.floor(Math.random() * 255);
 
-let address = '';
+let address = 'cdn.anycast.eu.org';
 let portWithRandomLog = '';
 const log = (/** @type {string} */ info, /** @type {string | undefined} */ event) => {
     console.log(`[${address}:${portWithRandomLog}] ${info}`, event || '');
